16a9f6b23d58510623a687a9d535662e8ebc79f6
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-20 14:31:47 +0530

    Ganesh Adding Insertion sorting program file

---
+++ InsertionSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAndAlgorithm
+{
+    class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 10, 12, 63, 40, 5, 16, 7, 48, 39, 100 };
+            int temp = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
